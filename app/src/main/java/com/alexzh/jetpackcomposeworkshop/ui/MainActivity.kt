e1d028bdb2d39b80a3e218279806efdd4b77873c
Alex Zhukovich <zhukovich.a@gmail.com>
2020-03-12 23:07:41 +0100

    Removed unused imports from MainActivity class

--- app/src/main/java/com/alexzh/jetpackcomposeworkshop/ui/MainActivity.kt
+++ app/src/main/java/com/alexzh/jetpackcomposeworkshop/ui/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.alexzh.jetpackcomposeworkshop.ui
 
 import android.os.Bundle
-import android.transition.Scene
-import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.animation.Crossfade
-import androidx.ui.core.Alignment
-import androidx.ui.core.Text
 import androidx.ui.core.setContent
-import androidx.ui.graphics.painter.ImagePainter
-import androidx.ui.graphics.painter.Painter
-import androidx.ui.layout.Column
-import androidx.ui.layout.Container
-import androidx.ui.material.AppBarIcon
-import androidx.ui.material.BottomAppBar
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.TopAppBar
-import androidx.ui.res.imageResource
-import com.alexzh.jetpackcomposeworkshop.R
 import com.alexzh.jetpackcomposeworkshop.ui.coffeedetails.CoffeeDrinkDetailsScreen
 import com.alexzh.jetpackcomposeworkshop.ui.coffeedrinks.CoffeeDrinksScreen
 import com.alexzh.jetpackcomposeworkshop.ui.order.OrderCoffeeDrinkScreen
