70b3eefc8caca41d8b3ea5b5354aecf3c286b757
Alex Zhukovich <zhukovich.a@gmail.com>
2021-02-20 22:11:34 +0100

    Migrate CoffeeDrinkDetailsScreen to MVVM

---
+++ app/src/main/java/com/alexzh/coffeedrinks/ui/screen/coffeedetails/CoffeeDrinkDetailsViewModel.kt
@@ -0,0 +1,47 @@
+package com.alexzh.coffeedrinks.ui.screen.coffeedetails
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alexzh.coffeedrinks.data.CoffeeDrinkRepository
+import com.alexzh.coffeedrinks.ui.screen.coffeedetails.exception.NoCoffeeDrinkFoundException
+import com.alexzh.coffeedrinks.ui.screen.coffeedetails.mapper.CoffeeDrinkDetailMapper
+import com.alexzh.coffeedrinks.ui.screen.coffeedetails.model.CoffeeDrinkDetail
+import com.alexzh.coffeedrinks.ui.screen.coffeedetails.model.CoffeeDrinkDetailState
+import com.alexzh.coffeedrinks.ui.state.UiState
+import kotlinx.coroutines.launch
+
+class CoffeeDrinkDetailsViewModel(
+    private val repository: CoffeeDrinkRepository,
+    private val mapper: CoffeeDrinkDetailMapper
+) : ViewModel() {
+    private val _uiState: MutableLiveData<UiState<CoffeeDrinkDetailState>> = MutableLiveData(UiState.Loading)
+    val uiState: LiveData<UiState<CoffeeDrinkDetailState>>
+        get() = _uiState
+
+    fun loadCoffeeDrinkDetails(coffeeDrinkId: Long) {
+        viewModelScope.launch {
+            _uiState.value = UiState.Loading
+            val coffeeDrink = mapper.map(
+                repository.getCoffeeDrink(coffeeDrinkId)
+            )
+            if (coffeeDrink != null) {
+                _uiState.value = UiState.Success(
+                    CoffeeDrinkDetailState(
+                        coffeeDrink
+                    )
+                )
+            } else {
+                _uiState.value = UiState.Error(NoCoffeeDrinkFoundException())
+            }
+        }
+    }
+
+    fun changeFavouriteState(coffeeDrink: CoffeeDrinkDetail) {
+        viewModelScope.launch {
+            repository.updateFavouriteState(coffeeDrink.id, !coffeeDrink.isFavourite)
+        }
+        loadCoffeeDrinkDetails(coffeeDrink.id)
+    }
+}
---
+++ app/src/main/java/com/alexzh/coffeedrinks/ui/screen/coffeedetails/model/CoffeeDrinkDetailState.kt
@@ -0,0 +1,5 @@
+package com.alexzh.coffeedrinks.ui.screen.coffeedetails.model
+
+data class CoffeeDrinkDetailState(
+    val coffeeDrinks: CoffeeDrinkDetail
+)
